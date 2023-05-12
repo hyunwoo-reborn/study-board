3c3282f410f9153e9789598266d766804acf1c56
hyunwoo.park <hyunwoo.park@gcar.co.kr>
2023-05-12 14:54:08 +0900

    #2 - 게시글 관련 뷰 테스트 작성
    우선 어떤 경로로 뷰를 열지 테스트로 명세를 뽑는다.

---
+++ src/main/java/com/study/board/controller/ArticleController.java
@@ -0,0 +1,13 @@
+package com.study.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/articles")
+@Controller
+public class ArticleController {
+
+
+
+}
