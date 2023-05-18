6b1ffcecc00ca77ecdf2100917bc325e69b029cc
hyunwoo.park <hyunwoo.park@gcar.co.kr>
2023-05-18 17:00:36 +0900

    #2 - 게시글 뷰 기본 골격 작성
    컨트롤러, 뷰, 테스트까지 한 번에 적용

--- src/test/java/com/study/board/controller/ArticleControllerTest.java
+++ src/test/java/com/study/board/controller/ArticleControllerTest.java
@@ -35,7 +35,6 @@ class ArticleControllerTest {
 
     }
 
-    @Disabled("구현중")
     @DisplayName("[view][GET] 게시글 상세 페이지 - 정상 호출")
     @Test
     public void givenNothing_whenRequestingArticleView_thenReturnArticleView() throws Exception {
@@ -46,8 +45,8 @@ class ArticleControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/detail"))
-                .andExpect(model().attributeExists("articles"))
-                .andExpect(model().attributeExists("articlesComments"));
+                .andExpect(model().attributeExists("article"))
+                .andExpect(model().attributeExists("articleComments"));
 
     }
 
