5a340dd2779b7a48583e25ef581e439b3a21bb32
hyunwoo.park <hyunwoo.park@gcar.co.kr>
2023-06-22 11:39:03 +0900

    #10 - 게시글 서비스 로직의 테스트와 골격 잡기
    다음의 기능을 테스트로 표현
    
    * 게시글 검색하면 게시글 리스트 반환
    * 게시글을 ID로 조회
    * 게시글 생성
    * 게시글 수정
    * 게시글 삭제
    
    필요한 dto 와 enum을 함께 작성했는데,
    추후에 수정할 수도 있음

---
+++ src/main/java/com/study/board/service/ArticleService.java
@@ -0,0 +1,42 @@
+package com.study.board.service;
+
+import com.study.board.domain.type.SearchType;
+import com.study.board.dto.ArticleDto;
+import com.study.board.dto.ArticleUpdateDto;
+import com.study.board.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Transactional
+@RequiredArgsConstructor
+@Service
+public class ArticleService {
+
+    private final ArticleRepository articleRepository;
+
+
+    @Transactional(readOnly = true)
+    public Page<ArticleDto> searchArticles(SearchType searchType, String searchKeyword) {
+        return Page.empty();
+    }
+
+    @Transactional(readOnly = true)
+    public ArticleDto searchArticle(long l) {
+        return null;
+    }
+
+    public void saveArticle(ArticleDto dto) {
+
+    }
+
+    public void updateArticle(long articleId, ArticleUpdateDto dto) {
+    }
+
+    public void deleteArticle(long articleId) {
+    }
+}
